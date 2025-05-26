1c3b3fd6a8eb43853b15726285114978ceacd5e3
sefer.memis <sefer.memis@getir.com>
2025-05-26 14:43:10 +0300

    first commit project init

---
+++ src/main/java/com/sefer/product_service/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.sefer.product_service.repository;
+
+import com.sefer.product_service.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductRepository extends MongoRepository<Product, String> {
+}
